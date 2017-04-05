3030b69f7bc4c5db28bb2ebe2307b9c52483ec19
slkk <644187060@qq.com>
2017-04-05 21:29:42 +0800

    complete the gradeBook with param

---
+++ How_to_program_Demo/GradeBook.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class GradeBook {
+public:
+	void displayMessage(string courseName) const {
+		cout << "welcome to gradeBook " + courseName << endl;
+	}
+};
+
+int main() {
+	string courseName;
+	GradeBook gradeBook;
+	cout << "please enter the your course name" << endl;
+	getline(cin,courseName);
+	gradeBook.displayMessage(courseName);
+
+}
